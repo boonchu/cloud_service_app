c87283a28fd88826795d7122e99a288685a0fe04
root <root@sandbox.hortonworks.com>
2015-09-02 15:48:14 +0000

    MP2 code

--- MP2.java
+++ MP2.java
@@ -1,10 +1,9 @@
-package MP1;
-
-import java.io.File;
+import java.io.*;
 import java.lang.reflect.Array;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.*;
+import java.util.Comparator;
 
 public class MP1 {
     Random generator;
@@ -36,7 +35,7 @@ public class MP1 {
     }
 
     Integer[] getIndexes() throws NoSuchAlgorithmException {
-        Integer n = 10000;
+        Integer n = 10000; 
         Integer number_of_lines = 50000;
         Integer[] ret = new Integer[n];
         this.initialRandomGenerator(this.userName);
@@ -51,12 +50,26 @@ public class MP1 {
         this.inputFileName = inputFileName;
     }
 
+    public int indexOf(int[] array, int key) {
+        int returnvalue = -1;
+        for (int i = 0; i < array.length; ++i) {
+            if (key == array[i]) {
+                returnvalue = i;
+                break;
+            }
+        }
+        return returnvalue;
+    }
+
     public String[] process() throws Exception {
         String[] ret = new String[20];
-      	int index = -1; 
-	String[] lines = new String[50000];
+        int index = -1;
+        Integer[] indexes = getIndexes(); 
+
+        String[] lines = new String[50000];
 
         try(BufferedReader br = new BufferedReader(new FileReader(inputFileName))) {
+
             for(String line; (line = br.readLine()) != null; ) {
                 index += 1;
                 if (Arrays.asList(indexes).indexOf(index) == -1) continue;
@@ -65,9 +78,38 @@ public class MP1 {
         }
         SortedMap<String, Integer> tokens = new TreeMap<String, Integer>();
 
+        for (Integer i : indexes) {
+            StringTokenizer st = new StringTokenizer(lines[i], delimiters);
+            while (st.hasMoreElements()) {
+                String str = st.nextElement().toString().toLowerCase().trim();
+                if (Arrays.asList(stopWordsArray).indexOf(str) == -1){
+                    if (tokens.containsKey(str))
+                        tokens.put(str, tokens.get(str) + 1);
+                    else
+                        tokens.put(str, 1);
+                }
+            }
+        }
+
+        tokens = SortByValue(tokens); 
+
+        int count = 0;
+        for (Map.Entry<String, Integer> entry:tokens.entrySet()) {
+            if (count >= 20) break;
+
+            ret[count] = entry.getKey();
+            count++;
+        }
 
         return ret;
     }
+
+    public TreeMap<String, Integer> SortByValue(SortedMap<String, Integer> map) {
+		ValueComparator vc =  new ValueComparator(map);
+		TreeMap<String,Integer> sortedMap = new TreeMap<String,Integer>(vc);
+		sortedMap.putAll(map);
+		return sortedMap;
+	}
 
     public static void main(String[] args) throws Exception {
         if (args.length < 1){
@@ -75,12 +117,29 @@ public class MP1 {
         }
         else {
             String userName = args[0];
-            String inputFileName = args[1];
+            String inputFileName = "./input.txt";
             MP1 mp = new MP1(userName, inputFileName);
             String[] topItems = mp.process();
             for (String item: topItems){
                 System.out.println(item);
             }
         }
+    }
+}
+
+class ValueComparator implements Comparator<String> {
+ 
+    Map<String, Integer> map;
+ 
+    public ValueComparator(Map<String, Integer> base) {
+        this.map = base;
+    }
+ 
+    public int compare(String a, String b) {
+        if (map.get(a) > map.get(b)) {
+            return -1;
+        } else {
+            return 1;
+        } // returning 0 would merge keys 
     }
 }
